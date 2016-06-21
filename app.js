7bcc916c941cb51ad5bb550d95a0bee1aa93474d
jonwilsoncomedy <jonwilsoncomedy@gmail.com>
2016-06-21 14:39:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 var provided = 'Hello World!';
+
+var n = provided.indexOf("e");
+console.log(n);
+
+var answer2 = n - provided;
+console.log(answer2);
+
+var answer3 = provided.substring(0, 5).split('');
+console.log(answer3);
+
+var answer4 = typeof(provided);
+console.log(answer4)
+
+var answer5 = provided.slice(-10).indexOf("!");
+console.log(answer5);
+
+var answer6 = provided.charAt(1);
+console.log(answer6);
+
+var answer7 = provided.toUpperCase();
+console.log(answer7);
+
+var answer8 = provided.replace('!', '?');
+console.log(answer8);
